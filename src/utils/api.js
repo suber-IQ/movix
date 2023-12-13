fc9cc3d8a7aa46cfd6c4d357d24f82e1d45102b8
sumit <suberquercusumit@gmail.com>
2023-12-13 16:47:48 +0530

    dotenv fix again

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "http://api.themoviedb.org/3";
 
-// const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 // const TMDB_KEY = import.meta.env.VITE_APP_TMDB_API_KEY;
-const TMDB_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWUzYjg4MzU2OGYxOWQ1YzFlODYxZDU3ZjAwNDNiMiIsInN1YiI6IjYxZGZmYTVlOTBiODdlMDA2ZDA5NDhlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9977sdQCF016EoSrjJAnHqASttkaO1fklE1krTvRRFU';
+// const TMDB_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWUzYjg4MzU2OGYxOWQ1YzFlODYxZDU3ZjAwNDNiMiIsInN1YiI6IjYxZGZmYTVlOTBiODdlMDA2ZDA5NDhlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9977sdQCF016EoSrjJAnHqASttkaO1fklE1krTvRRFU';
 
 
 const headers = {
@@ -19,7 +19,7 @@ export const fetchDataFromApi = async (url,params) => {
        });
        return data;
    } catch (error) {
-      console.log(error);
+      // console.log(error);
       return error;
    }
 }

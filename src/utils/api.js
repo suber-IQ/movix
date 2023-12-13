186fe5d7912a39008cdc9e9e30133c199c417926
sumit <suberquercusumit@gmail.com>
2023-12-13 17:00:18 +0530

    fix authorization into header

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const BASE_URL = "http://api.themoviedb.org/3";
 
-const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 // const TMDB_KEY = import.meta.env.VITE_APP_TMDB_API_KEY;
 // const TMDB_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWUzYjg4MzU2OGYxOWQ1YzFlODYxZDU3ZjAwNDNiMiIsInN1YiI6IjYxZGZmYTVlOTBiODdlMDA2ZDA5NDhlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9977sdQCF016EoSrjJAnHqASttkaO1fklE1krTvRRFU';
 
 
 const headers = {
-      Authorization: `Bearer ${TMDB_KEY}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWUzYjg4MzU2OGYxOWQ1YzFlODYxZDU3ZjAwNDNiMiIsInN1YiI6IjYxZGZmYTVlOTBiODdlMDA2ZDA5NDhlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9977sdQCF016EoSrjJAnHqASttkaO1fklE1krTvRRFU`,
 }
 
 export const fetchDataFromApi = async (url,params) => {
